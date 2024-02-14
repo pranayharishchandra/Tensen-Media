9b7d2f41747fa860d220ca775cc8a73181ed96c8
pranay <pranayharishchandra@gmail.com>
2024-02-14 21:05:47 +0530

    successful authontication using clerk

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,12 @@
+// documentation link: https://clerk.com/docs/quickstarts/nextjs
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      onboarding page
+    </div>
+  )
+}
+
+export default page
---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,8 @@
+// https://clerk.com/docs/references/nextjs/custom-signup-signin-pages
+
+import { SignUp } from "@clerk/nextjs";
+
+export default function Page() {
+  return <SignUp />;
+}
+ 
